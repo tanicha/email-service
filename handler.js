87126f0fd70b6a778ba73f9fcb2049494a1dc567
Tani Cha <tanijcha@gmail.com>
2024-01-02 12:13:29 -0800

    ses permissions

--- handler.js
+++ handler.js
@@ -1,18 +1,36 @@
+const AWS = require('aws-sdk');
+const ses = new AWS.SES({ region: 'us-west-2' });
+
 'use strict';
 
 module.exports.sendEmail = async (event) => {
+  const params = {
+    Destination: {
+      ToAddresses: ['tanijcha@gmail.com'], 
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: 'This is a message generated automatically from a Lambda function.',
+        },
+      },
+      Subject: {
+        Data: 'Hello from Lambda',
+      },
+    },
+    Source: 'tanijcha@gmail.com', 
+  };
+  await ses.sendEmail(params).promise();
+
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: `Email sent to ${params.Destination.ToAddresses}`,
         input: event,
       },
       null,
       2
     ),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
